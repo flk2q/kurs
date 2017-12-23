e279c7f9eab08f85982cb0c78f70441585461a79
flk2q <34782697+flk2q@users.noreply.github.com>
2017-12-23 04:24:03 +0300

    Add files via upload

---
+++ TaskManager/TaskManager/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+     
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.kIllProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kIllProcessToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(134, 26);
+            // 
+            // kIllProcessToolStripMenuItem
+            // 
+            this.kIllProcessToolStripMenuItem.Name = "kIllProcessToolStripMenuItem";
+            this.kIllProcessToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.kIllProcessToolStripMenuItem.Text = "KIll Process";
+            this.kIllProcessToolStripMenuItem.Click += new System.EventHandler(this.kIllProcessToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Завершить процесс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Запущенные процессы:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(155, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(534, 13);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(81, 23);
+            this.btnsearch.TabIndex = 4;
+            this.btnsearch.Text = "Поиск";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(495, 45);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(157, 20);
+            this.txtsearch.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(929, 433);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(737, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 523);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "TM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kIllProcessToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ TaskManager/TaskManager/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Process[] procs;
+        private void GetProcesses()
+        {
+            procs = Process.GetProcesses();
+            if (Convert.ToInt32(label2.Text) != procs.Length) // Проверяем, были ли запущены или завершены новые процессы
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("ID Процесса"+
+                       "    \t    \t      \t Ответ процесса: " +
+                       " \t   \t   \t         Объем выделяемой памяти в " + " байтах" +
+                       " \t   \t   \t Имя процесса: ");
+                for (int i = 0; i < procs.Length; i++)
+                {
+                    listBox1.Items.Add((i+1)+") " + procs[i].Id + 
+                        "          \t    \t    | \t \t " +procs[i].Responding +
+                        "          \t    \t    | \t \t " + procs[i].WorkingSet64 + 
+                        "          \t    \t    | \t \t " + procs[i].ProcessName);
+                    // Добавить имя процесса в список
+                    
+                    //listBox1.Items.Add(procs[i].Id);
+                    // listBox1.Items.Add(procs[i].WorkingSet64);
+                }
+                label2.Text = procs.Length.ToString();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetProcesses();
+        }
+
+        // Проверяем каждую секунду  изменениями в списке процессов
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetProcesses();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            DialogResult result = MessageBox.Show(
+          "Завершить данный процесс?",
+          "Предупреждение",
+          MessageBoxButtons.YesNo,
+          MessageBoxIcon.Information,
+          MessageBoxDefaultButton.Button1,
+          MessageBoxOptions.DefaultDesktopOnly);
+
+            if (result == DialogResult.Yes)
+                 procs[listBox1.SelectedIndex-1].Kill(); // Завершаем процесс, соответствующий выбранному индексу в списке;
+
+           
+        }
+
+        private void kIllProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            procs[listBox1.SelectedIndex-1].Kill();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItems.Clear();
+            for (int i = listBox1.Items.Count - 1; i >= 0;i--)
+            {
+                if (listBox1.Items[i].ToString().ToLower().Contains(txtsearch.Text.ToLower()))
+                    listBox1.SetSelected(i, true);
+
+            }
+            DialogResult result = MessageBox.Show(
+         "Найдено результатов поиска: " + listBox1.SelectedItems.Count.ToString(),
+         "Поиск завершен",
+         MessageBoxButtons.OKCancel,
+         MessageBoxIcon.Information,
+         MessageBoxDefaultButton.Button1,
+         MessageBoxOptions.DefaultDesktopOnly);
+           // label3.Text = "Найдено результатов поиска: "+ listBox1.SelectedItems.Count.ToString();
+               }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
